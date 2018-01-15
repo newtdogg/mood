82f8565cc4205ace9d1aff770776bfc8293cfcc8
hoskingl <hoskinglouis@gmail.com>
2018-01-15 22:14:37 +0000

    Added crowd-pleasing feature

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
-    }))
+    }));
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
@@ -34,10 +34,28 @@ function createWindow () {
     })
 }
 
+function openbestwebsite() {
+    win = new BrowserWindow({width: 800, height: 600});
+
+    win.webContents.on('did-finish-load', () => {
+        win.show();
+        win.focus();
+    });
+    win.loadURL('http://www.louishosking.uk');
+    win.on('closed', function() {
+        win = null
+    })
+}
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+        createWindow()
+        openbestwebsite()
+    }
+);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -46,7 +64,7 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
@@ -54,7 +72,8 @@ app.on('activate', function () {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
